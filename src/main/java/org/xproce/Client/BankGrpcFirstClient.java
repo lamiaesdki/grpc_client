593ab35909151891af5fe23770c229882dfe3707
lamia <Sadki_lamiae@emsi-edu.ma>
2024-11-28 22:08:09 +0100

    first commit

---
+++ src/main/java/org/xproce/Client/BankGrpcFirstClient.java
@@ -0,0 +1,26 @@
+package org.xproce.Client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.xproce.stubs.Bank;
+import org.xproce.stubs.BankServiceGrpc;
+public class BankGrpcFirstClient {
+    public static void main(String[] args) {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",5555)
+
+                .usePlaintext()
+                .build();
+
+        BankServiceGrpc.BankServiceBlockingStub blockingStub =
+                BankServiceGrpc.newBlockingStub(managedChannel);
+        Bank.ConvertCurrencyRequest request = Bank.ConvertCurrencyRequest.newBuilder()
+
+                .setCurrencyFrom("MAD")
+                .setCurrencyTo("Euro")
+                .setAmount(15425)
+                .build();
+
+        Bank.ConvertCurrencyResponse response = blockingStub.convert(request);
+        System.out.println(response);
+    }
+}
